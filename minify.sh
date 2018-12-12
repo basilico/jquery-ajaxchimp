ef1e6d4ec383c2cf6cde16b657f8af68288fd8c5
giorgio <giorgio@basili.co>
2018-12-12 11:59:07 +0100

    remove src and move to dist

--- minify.sh
+++ minify.sh
@@ -4,5 +4,5 @@
 # npm install uglify-js -g 
 #
 
-uglifyjs src/jquery.ajaxchimp.js -o dist/jquery.ajaxchimp.min.js
-uglifyjs src/jquery.ajaxchimp.langs.js -o dist/jquery.ajaxchimp.langs.min.js
+uglifyjs dist/jquery.ajaxchimp.js -o dist/jquery.ajaxchimp.min.js
+uglifyjs dist/jquery.ajaxchimp.langs.js -o dist/jquery.ajaxchimp.langs.min.js
